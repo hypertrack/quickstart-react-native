64c66e5cbe3bd9fc7ecb5dc66a44ecec86a655be
deniszpua <quux@ukr.net>
2020-01-08 12:23:50 +0200

    Update package names

--- android/app/src/main/java/com/hypertrack/rnquickstart/MainApplication.java
+++ android/app/src/main/java/com/hypertrack/rnquickstart/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.hypertrack.quickstart.react.android.github.BuildConfig;
 import com.hypertrack.reactnative.androidsdk.HTSDKPackage;
 
 import java.util.Arrays;
