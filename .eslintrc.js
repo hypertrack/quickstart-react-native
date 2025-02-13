649f3f339a432e9432ea53c6017dee76e172aa3e
Pavel Kuznetsov <pavel@hypertrack.io>
2025-02-13 19:54:09 +0200

    Revert "Remove"
    022c031b65dcd02435274a10295bb81bbb088dbb

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'react-hooks/exhaustive-deps': 'off',
+        'prettier/prettier': 'off',
+      },
+    },
+  ],
+};
