a51c41ae2f891f3f33bebfac52200368416f6a81
Pavel Kuznetsov <pavel@hypertrack.io>
2025-02-14 20:41:30 +0200

    Restore manual

--- android/app/src/main/java/com/quickstartreactnative/MainApplication.kt
+++ android/app/src/main/java/com/quickstartreactnative/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.quickstartreactnative.BuildConfig
 
 class MainApplication : Application(), ReactApplication {
 
